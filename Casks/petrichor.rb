fd846ed066af160f7a10f6b4243ede55696e73d6
Kushal Pandya <kushalspandya@gmail.com>
2025-08-16 14:44:29 -0400

    Publish 1.0.0-beta-4

--- Casks/petrichor.rb
+++ Casks/petrichor.rb
@@ -1,25 +1,25 @@
 cask "petrichor" do
-  version "1.0.0-beta-3"
-  sha256 "c6cca44334905042380f52f788aeec6cbcb5f3ada20317bc8bab0c2b9604b446"
+  version "1.0.0-beta-4"
+  sha256 "c6e095ee30ffaf8969343f0387046b68f127d042e032ab414407d6fb7104c7d3"
 
   url "https://github.com/kushalpandya/Petrichor/releases/download/v#{version}/Petrichor-#{version}-Universal.dmg"
   name "Petrichor"
   desc "An offline macOS music player"
   homepage "https://github.com/kushalpandya/Petrichor"
+  auto_updates true
 
   livecheck do
     url :url
     strategy :github_latest
-    regex(/^v?(\d+(?:\.\d+)+(?:-alpha-\d+)?)$/i)
+    regex(/^v?(\d+(?:\.\d+)+(?:-beta-\d+)?)$/i)
   end
 
   app "Petrichor.app"
 
   zap trash: [
-    "~/Library/Application Support/org.Petrichor",
+    "~/Library/Containers/org.Petrichor",
     "~/Library/Preferences/org.Petrichor.plist",
     "~/Library/Saved Application State/org.Petrichor.savedState",
-    "~/Library/Caches/org.Petrichor",
   ]
 
   caveats <<~EOS
@@ -28,8 +28,5 @@ cask "petrichor" do
     To open for the first time:
     1. Right-click the app and select "Open"
     2. Click "Open" in the dialog
-    
-    If you see "Petrichor cannot be opened" error:
-      xattr -dr com.apple.quarantine /Applications/Petrichor.app
   EOS
 end
